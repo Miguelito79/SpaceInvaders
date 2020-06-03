277f479ae4e1f00cfab76828fef6fe55fe1bf0a4
Miguelito79 <66297480+Miguelito79@users.noreply.github.com>
2020-06-03 18:19:03 +0200

    Diagnostic console
    Console used to test the cpu core with a series of diagnotic programs ezecuted by the emulaator

---
+++ SInvader_Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using SInvader_Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInvader_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emulator emulator = Emulator.Instance;
+            //emulator.Run(@"C:\Test\spaceinvaders\i8080_Disassembler\Test\cpudiag.bin", 0x100);
+            //emulator.Run(@"C:\Test\spaceinvaders\i8080_Disassembler\Test\8080ex1.com", 0x100);
+            //emulator.Run(@"C:\Test\spaceinvaders\i8080_Disassembler\Test\8080exer.com", 0x100);            
+            //emulator.Run(@"C:\Test\spaceinvaders\i8080_Disassembler\Test\8080exm.com", 0x100);
+            //emulator.Run(@"C:\Test\spaceinvaders\i8080_Disassembler\Test\cputest.com", 0x100);
+            //emulator.Run(@"C:\Test\spaceinvaders\i8080_Disassembler\Test\tst8080.com", 0x100);
+        }
+    }
+}
