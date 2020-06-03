309d93ee5d629722cf30e4c900a4bbe530a3aded
Miguelito79 <66297480+Miguelito79@users.noreply.github.com>
2020-06-03 18:12:13 +0200

    Main project

---
+++ SInvader/Main.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SInvader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runSpaceInvadersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.menuStrip, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.300395F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.69961F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(451, 419);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(451, 24);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runSpaceInvadersToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // runSpaceInvadersToolStripMenuItem
+            // 
+            this.runSpaceInvadersToolStripMenuItem.Name = "runSpaceInvadersToolStripMenuItem";
+            this.runSpaceInvadersToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.runSpaceInvadersToolStripMenuItem.Text = "Run Space Invaders";
+            this.runSpaceInvadersToolStripMenuItem.Click += new System.EventHandler(this.runSpaceInvaders_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(3, 37);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(445, 379);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 419);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Space Invaders";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Main_KeyUp);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        public System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runSpaceInvadersToolStripMenuItem;
+    }
+}
+
---
+++ SInvader/Main.cs
@@ -0,0 +1,99 @@
+﻿using SInvader_Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SInvader
+{
+    public partial class Main : Form
+    {        
+        Emulator _emulator = Emulator.Instance;        
+
+        public Main()
+        {
+            InitializeComponent();
+
+            _emulator.OnDrawImage += _emulator_OnDrawImage;
+            _emulator.OnEmulatorStateChange += _emulator_OnEmulatorStateChange;
+        }
+
+        private void _emulator_OnEmulatorStateChange(Emulator.TCurrentState state)
+        {
+            if (state == Emulator.TCurrentState.STOPPED)
+            {
+                if (this.InvokeRequired)
+                    this.Invoke((MethodInvoker)delegate { this.Close(); });
+                else
+                    this.Close();
+            }                
+        }
+
+        private void _emulator_OnDrawImage(Image image)
+        {
+            if (this.InvokeRequired)
+                this.Invoke((MethodInvoker)delegate
+                   {
+                       pictureBox.Image = _emulator.GPU.Display;
+                       pictureBox.Refresh();
+                   });
+            else
+            {
+                pictureBox.Image = _emulator.GPU.Display;
+                pictureBox.Refresh();
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            _emulator.KeyDown(e.KeyValue);
+        }
+
+        private void Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            _emulator.KeyUp(e.KeyValue);
+        }
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            pictureBox.Image = _emulator.GPU.Display;
+        }
+        
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_emulator.CurrentState == Emulator.TCurrentState.RUNNING)
+            {
+                _emulator.StopEmulation();
+                e.Cancel = true;
+            }                
+        }
+
+        private void runSpaceInvaders_Click(object sender, EventArgs e)
+        {
+            List<string> roms = new List<string>();
+            roms.Add(@"C:\Temp\games\spaceinvaders\invaders.h");
+            roms.Add(@"C:\Temp\games\spaceinvaders\invaders.g");
+            roms.Add(@"C:\Temp\games\spaceinvaders\invaders.f");
+            roms.Add(@"C:\Temp\games\spaceinvaders\invaders.e");
+
+            _emulator.Run(roms.ToArray());
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    _emulator.StopEmulation();
+        //}        
+    }
+}
